8fa0fa9d1d5bfa3cb313037b9610bf7a78a87f53
unknown <jacqui.longhurst@btinternet.com>
2022-07-25 01:48:20 +0200

    Alternative solution

--- 7kyu/disembowel-trolls.js
+++ 7kyu/disembowel-trolls.js
@@ -11,18 +11,25 @@
 // My solution:
 
 
-function disemvowel(str) {
-    console.log(typeof str)
-    let newStr = ''
-    for ( s of str ) {
-      if ( s === 'a' || s === 'e' || s === 'i' || s === 'o' || s ==='u') {
-        continue
-      } else if ( s === 'A' || s === 'E' || s === 'I' || s === 'O' || s ==='U') {
-        continue
-      } else {
-        newStr += s;
-      console.log(newStr)
-      } 
-    }
-    return newStr;
-  } 
+// function disemvowel(str) {
+//     console.log(typeof str)
+//     let newStr = ''
+//     for ( s of str ) {
+//       if ( s === 'a' || s === 'e' || s === 'i' || s === 'o' || s ==='u') {
+//         continue
+//       } else if ( s === 'A' || s === 'E' || s === 'I' || s === 'O' || s ==='U') {
+//         continue
+//       } else {
+//         newStr += s;
+//       console.log(newStr)
+//       } 
+//     }
+//     return newStr;
+//   } 
+
+// or
+
+  function disemvowel(str) {
+    var diemvoweledStr = str.replace(/[aeiou]/gi,"");
+    return diemvoweledStr;
+  }
