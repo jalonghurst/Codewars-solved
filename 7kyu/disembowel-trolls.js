f545bc47b8d25502609531dbb108b014fe6d239f
unknown <jacqui.longhurst@btinternet.com>
2022-07-25 01:45:04 +0200

    remove vowels from a string value

---
+++ 7kyu/disembowel-trolls.js
@@ -0,0 +1,28 @@
+// Trolls are attacking your comment section!
+
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+
+
+// My solution:
+
+
+function disemvowel(str) {
+    console.log(typeof str)
+    let newStr = ''
+    for ( s of str ) {
+      if ( s === 'a' || s === 'e' || s === 'i' || s === 'o' || s ==='u') {
+        continue
+      } else if ( s === 'A' || s === 'E' || s === 'I' || s === 'O' || s ==='U') {
+        continue
+      } else {
+        newStr += s;
+      console.log(newStr)
+      } 
+    }
+    return newStr;
+  } 
